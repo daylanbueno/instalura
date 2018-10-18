d0546defe6744e66a67793ff211dc84f018025f5
Dailan Bueno <daylansantos@gmail.com>
2018-10-17 21:02:11 -0300

    Iniciando projeto já desenvolvido locamente.

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/reset.css';
+import './css/timeline.css';
+import './css/login.css'
+import App from './App';
+import Login from './componentes/Login';
+import {Router,Route} from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory'
+const newHistory = createBrowserHistory();
+
+
+
+ReactDOM.render(
+  (
+    <Router history={newHistory} >
+      <div>
+              <Route path="/" component={Login}/>
+              <Route path="/timeline" component={App} />
+     </div>
+    </Router>
+  ), 
+  document.getElementById('root')
+);
